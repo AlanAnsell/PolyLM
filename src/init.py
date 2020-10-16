cb49812cc03e7e1eee9204ba07b7ad384b7916c5
Alan Ansell <aa381@students.waikato.ac.nz>
2020-10-16 22:35:14 +1300

    tidy up command line params

--- src/init.py
+++ src/init.py
@@ -8,56 +8,110 @@ from data import Vocabulary
 logging.basicConfig(level=logging.INFO)
 
 # High-level options
-tf.app.flags.DEFINE_string("gpus", "0", "Which GPUs to use, e.g. '0 1'")
+tf.app.flags.DEFINE_string("gpus", "0", "Which GPUs to use, e.g. '0,1'")
 tf.app.flags.DEFINE_string("mode", "train", "Available modes: train")
-tf.app.flags.DEFINE_integer("n_batches", 1000000, "")
-tf.app.flags.DEFINE_integer("checkpoint_version", -1, "")
+tf.app.flags.DEFINE_integer(
+        "checkpoint_version", -1,
+        "Which saved model checkpoint version to load.")
 
 # Model hyperparameters
-tf.app.flags.DEFINE_integer("embedding_size", 128, "Size of word vectors.")
-tf.app.flags.DEFINE_bool("use_disambiguation_layer", True, "")
-tf.app.flags.DEFINE_bool("isolate_sharpening", False, "")
-tf.app.flags.DEFINE_integer("n_disambiguation_layers", 4, "Number of LSTM layers.")
-tf.app.flags.DEFINE_integer("n_prediction_layers", 8, "Number of LSTM layers.")
-tf.app.flags.DEFINE_integer("bert_intermediate_size", 512, "Dimension of LSTM hidden layer.")
-tf.app.flags.DEFINE_integer("n_attention_heads", 8, "Dimension of LSTM hidden layer.")
-tf.app.flags.DEFINE_integer("max_senses_per_word", 8, "Number of senses per word.")
-tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Clip gradients to this norm")
-tf.app.flags.DEFINE_float("cell_clip", 3.0, "Cell clip.")
-tf.app.flags.DEFINE_float("proj_clip", 3.0, "Proj clip.")
+tf.app.flags.DEFINE_integer(
+        "embedding_size", 128, "Size of sense embeddings.")
+tf.app.flags.DEFINE_integer(
+        "bert_intermediate_size", 512, "Number of filters.")
+tf.app.flags.DEFINE_integer(
+        "n_disambiguation_layers", 4,
+        "Number of Transformer blocks in disambiguation layer.")
+tf.app.flags.DEFINE_integer(
+        "n_prediction_layers", 8,
+        "Number of Transformer blocks in prediction layer.")
+tf.app.flags.DEFINE_integer(
+        "n_attention_heads", 8, "Number of attention heads.")
+tf.app.flags.DEFINE_bool(
+        "use_disambiguation_layer", True,
+        "Whether to have a disambiguation layer.")
+tf.app.flags.DEFINE_integer(
+        "max_senses_per_word", 8,
+        "Maximum number of senses a token may have.")
+tf.app.flags.DEFINE_float(
+        "ml_coeff", 0.1, "Coefficient of match loss.")
+tf.app.flags.DEFINE_float(
+        "dl_r", 1.5, "Distinctness loss coefficient r.")
 tf.app.flags.DEFINE_float("dropout", 0.1, "Dropout probability.")
-tf.app.flags.DEFINE_float("ml_coeff", 0.1, "")
-tf.app.flags.DEFINE_float("dl_r", 1.5, "")
 
 # Training hyperparameters
-tf.app.flags.DEFINE_float("learning_rate", 0.0001, "Learning rate.")
-tf.app.flags.DEFINE_integer("lr_warmup_steps", 10000, "Learning rate.")
-tf.app.flags.DEFINE_bool("anneal_lr", True, "")
+tf.app.flags.DEFINE_integer(
+        "n_batches", 6000000, "Number of batches to train for.")
+tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size per GPU.")
+tf.app.flags.DEFINE_integer("max_seq_len", 128, "Maximum sequence length.")
+tf.app.flags.DEFINE_float(
+        "max_gradient_norm", 5.0, "Clip gradients to this norm.")
+tf.app.flags.DEFINE_float("learning_rate", 0.00003, "Learning rate.")
+tf.app.flags.DEFINE_integer(
+        "lr_warmup_steps",
+        10000,
+        "Number of batches over which learning rate is linearly increased at "
+        "start of training.")
+tf.app.flags.DEFINE_bool(
+        "anneal_lr", True, "Whether to linearly decay learning rate during "
+        "training.")
 
-tf.app.flags.DEFINE_integer("dl_warmup_steps", 1000000, "Learning rate.")
-tf.app.flags.DEFINE_integer("ml_warmup_steps", 1000000, "Learning rate.")
-tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size to use")
-tf.app.flags.DEFINE_integer("max_seq_len", 128, "Maximum sentence length")
+tf.app.flags.DEFINE_integer(
+        "dl_warmup_steps", 2000000,
+        "Number of batches over which distinctness loss exponent r is linearly "
+        "increased at beginning of training.")
+tf.app.flags.DEFINE_integer(
+        "ml_warmup_steps", 1000000,
+        "Number of batches over which match loss coefficient is linearly "
+        "increased at beginning of training.")
+tf.app.flags.DEFINE_float(
+        "mask_prob", 0.15, "The proportion of tokens which are replaced with "
+        "[MASK].")
+tf.app.flags.DEFINE_bool(
+        "bert_masking", True,
+        "Whether to use BERT's masking policy, i.e. 80% of MLM target tokens "
+        "are replaced with [MASK], 10% are replaced with a random token and "
+        "10% are unchanged. If false, all target tokens are replaced with "
+        "[MASK].")
 
 # Preprocessing parameters
-tf.app.flags.DEFINE_integer("min_occurrences_for_vocab", 500, "Minimum number of times a token needs to occur in the corpus to be included in the vocabulary")
-tf.app.flags.DEFINE_integer("min_occurrences_for_polysemy", 20000, "Minimum number of times a token needs to occur in the corpus to be considered for having multiple senses.")
-tf.app.flags.DEFINE_float("mask_prob", 0.15, "The proportion of tokens which are replaced with <MASK>.")
-tf.app.flags.DEFINE_bool("bert_masking", True, "")
-tf.app.flags.DEFINE_bool("lemmatize", True, "")
+tf.app.flags.DEFINE_integer(
+        "min_occurrences_for_vocab", 500,
+        "Minimum number of times a token needs to occur in the corpus to be "
+        "included in the vocabulary.")
+tf.app.flags.DEFINE_integer(
+        "min_occurrences_for_polysemy", 20000,
+        "Minimum number of times a token needs to occur in the corpus to be "
+        "considered for having multiple senses.")
+tf.app.flags.DEFINE_bool(
+        "lemmatize", True, "Whether to lemmatize during preprocessing.")
 
 # Display parameters
-tf.app.flags.DEFINE_string("test_words", "", "Words to display most similar vectors for/words to find senses for.")
-tf.app.flags.DEFINE_integer("print_every", 100, "How many batches to do per print.")
-tf.app.flags.DEFINE_integer("save_every", 10000, "How many iterations to do per save.")
-tf.app.flags.DEFINE_integer("test_every", 500, "How many batches to do per display of test words.")
-tf.app.flags.DEFINE_integer("keep", 1, "How many checkpoints to keep. 0 indicates keep all (you shouldn't need to do keep all though - it's very storage intensive).")
+tf.app.flags.DEFINE_string(
+        "test_words", "",
+        "Words to display neighbouring embeddings for during training.")
+tf.app.flags.DEFINE_integer(
+        "print_every", 100, "How many batches per stats print-out.")
+tf.app.flags.DEFINE_integer(
+        "save_every", 10000, "How many batches per save.")
+tf.app.flags.DEFINE_integer(
+        "test_every", 500, "How many batches per display of test words.")
+tf.app.flags.DEFINE_integer(
+        "keep", 1, "How many checkpoints to keep.")
+
 
 # File and directory parameters
-tf.app.flags.DEFINE_string("extra_multisense_vocab", "", "")
-tf.app.flags.DEFINE_string("train_dir", "", "Training directory to save the model parameters and other info.")
-tf.app.flags.DEFINE_string("corpus_path", "", "Where to find preprocessed corpus data for training.")
-tf.app.flags.DEFINE_string("vocab_path", "", "Where to find vocab file for training.")
+tf.app.flags.DEFINE_string(
+        "extra_multisense_vocab", "",
+        "File containing multisense tokens - overrides "
+        "min_occurrences_for_polysemy.")
+tf.app.flags.DEFINE_string(
+        "train_dir", "",
+        "Training directory to save the model parameters and other info.")
+tf.app.flags.DEFINE_string(
+        "corpus_path", "", "Text file containing corpus.")
+tf.app.flags.DEFINE_string(
+        "vocab_path", "", "Vocab file.")
 
 # Evaluation parameters
 tf.app.flags.DEFINE_string("sense_prob_source", "prediction", "")
@@ -88,6 +142,7 @@ tf.app.flags.DEFINE_bool("wsd_use_definitions", True, ".")
 tf.app.flags.DEFINE_bool("wsd_use_frequency_counts", True, ".")
 tf.app.flags.DEFINE_bool("wsd_ignore_semeval_07", True, ".")
 tf.app.flags.DEFINE_integer("wsd_max_examples_per_sense", 1000000, ".")
+
 
 def get_multisense_vocab(path, vocab, FLAGS):
     multisense_vocab = set(
