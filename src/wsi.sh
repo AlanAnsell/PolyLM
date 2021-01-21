e72fd0acb7f8aa44b4198b6591ef486897ba8596
Alan Ansell <aja63@cam.ac.uk>
2021-01-22 03:22:51 +1300

    add WSI evaluation scripts

--- src/wsi.sh
+++ src/wsi.sh
@@ -24,4 +24,18 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-python wsi.py --flagfile=$model_dir/flags --model_dir=$model_dir --batch_size=16 --gpus=$gpus --wsi_path=$wsi_dir --wsi_format=$format --lemmatize=$lemmatize --sense_prob_source=$sense_prob_source --checkpoint_version=$checkpoint_version --wsi_2013_thresh=$wsi_2013_thresh --pos_tagger_root=$pos_tagger_root
+OUTFILE=/tmp/wsi-predictions.txt
+
+python wsi.py --flagfile=$model_dir/flags --model_dir=$model_dir --batch_size=16 --gpus=$gpus --wsi_path=$wsi_dir --wsi_format=$format --lemmatize=$lemmatize --sense_prob_source=$sense_prob_source --checkpoint_version=$checkpoint_version --wsi_2013_thresh=$wsi_2013_thresh --pos_tagger_root=$pos_tagger_root > $OUTFILE
+
+if [[ $format == "SemEval-2010" ]]; then
+    java -jar wsi-eval/fscore.jar $OUTFILE $wsi_dir/evaluation/unsup_eval/keys/all.key all
+    java -jar wsi-eval/vmeasure.jar $OUTFILE $wsi_dir/evaluation/unsup_eval/keys/all.key all
+fi
+
+if [[ $format == "SemEval-2013" ]]; then
+    java -jar wsi-eval/fuzzy-bcubed.jar $wsi_dir/keys/gold/all.key $OUTFILE
+    java -jar wsi-eval/fuzzy-nmi.jar $wsi_dir/keys/gold/all.key $OUTFILE
+fi
+
+
